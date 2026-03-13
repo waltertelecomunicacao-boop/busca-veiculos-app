0c4f387b6748139f69c4ed683749bc174efe8466
waltertelecomunicacao-boop <waltertelecomunicacao@gmail.com>
2026-03-13 14:42:20 -0300

    Delete cadastro.php

--- cadastro.php
+++
@@ -1,8 +0,0 @@
-<form id="formCadastro">
-<input name="placa" placeholder="Placa">
-<input name="nome" placeholder="Proprietário">
-<input name="modelo" placeholder="Modelo">
-<button type="submit">Salvar</button>
-</form>
-
-<script src="js/app.js"></script>
